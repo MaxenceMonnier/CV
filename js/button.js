97660207665b1630e84685feb9f864681864ba76
MaxenceMonnier <maxencemonnier@gmail.com>
2023-10-31 11:03:05 +0100

    user-scalable=no

--- js/button.js
+++ js/button.js
@@ -1,4 +1,4 @@
-$(document).ready (function() {
+  $(document).ready (function() {
     $("#download").hover (function() {
     $("#list").toggleClass("hide");
     $("#downloadtray").toggleClass("downloadtray"); 
